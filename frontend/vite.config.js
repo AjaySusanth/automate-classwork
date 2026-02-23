f98b8013d947a4bfba933bb7fcd85e3c18b3e08a
AjaySusanth <ajaysusanth10@gmail.com>
2026-02-23 21:31:35 +0530

    feat: implemented get pending assignments and submission confirmation message

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true,              // 👈 VERY IMPORTANT
+    port: 5173,
+    allowedHosts: true
+  },
   plugins: [
     react(),
     tailwindcss()
   ],
+
 })
